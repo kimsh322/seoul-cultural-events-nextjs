d0613cfc84b4d204e21f49daccbb791b66ddce62
kimsh322 <kimsh91322@gmail.com>
2023-07-27 16:51:19 +0900

    Feat : Loading indicator 추가
    - Loading indicator 추가
    - useInfiniteQuery 부분 코드 보기좋게 수정

--- components/home/AllEvents.tsx
+++ components/home/AllEvents.tsx
@@ -1,27 +1,31 @@
 import { getAllEvents, getScrollEvents } from "@/util/fetch-events";
-import { dehydrate, QueryClient, useInfiniteQuery } from "@tanstack/react-query";
+import {
+  dehydrate,
+  GetNextPageParamFunction,
+  QueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
 import EventCard from "./EventCard";
 import { Box } from "@mui/material";
 import { AxiosError } from "axios";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import CircularProgress from "@mui/material/CircularProgress";
+
+// 다음 페이지 계산하는 콜백함수
+const getNextPageParam: GetNextPageParamFunction = (_, allPage) => {
+  const nextPage = (1 + allPage.length) * 12;
+  if (nextPage > 1000) return false;
+  return nextPage;
+};
 
 export default function AllEvents() {
   const { ref, inView } = useInView();
+
   // 무한 스크롤 구현
-  const { data, fetchNextPage, error, isError, status, isFetchingNextPage } = useInfiniteQuery<
-    any,
-    AxiosError
-  >({
-    queryKey: ["events"],
-    queryFn: getScrollEvents,
-    getNextPageParam: (_, allPage) => {
-      // 다음 페이지 계산
-      const nextPage = (1 + allPage.length) * 12;
-      if (nextPage > 1000) return false;
-      return nextPage;
-    },
-  });
+  // prettier-ignore
+  const { data, fetchNextPage, error, isError, status, isFetchingNextPage } = 
+  useInfiniteQuery<any, AxiosError>({queryKey: ["events"], queryFn: getScrollEvents, getNextPageParam});
 
   useEffect(() => {
     // ref가 view에 들어오면 함수 실행
@@ -30,7 +34,12 @@ export default function AllEvents() {
 
   if (isError) return <div>{error.message}</div>;
 
-  if (status === "loading") return <p>Loading...</p>;
+  if (status === "loading")
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
 
   return (
     <>
@@ -57,8 +66,12 @@ export default function AllEvents() {
           });
         })}
       </Box>
+      {isFetchingNextPage && (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </Box>
+      )}
       <div ref={ref} style={{ height: 50 }} />
-      {isFetchingNextPage ? <p>불러오는중...</p> : null}
     </>
   );
 }
