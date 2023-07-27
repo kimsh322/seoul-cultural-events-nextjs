31d2efee4f00eb3a5bf7026c3de0a1f074399033
kimsh322 <kimsh91322@gmail.com>
2023-07-27 12:41:42 +0900

    Fix : 무한스크롤 트리거 변경
    - react-intersection-observer 패키지설치
    - 무한스크롤 트리거를 useInview hook으로 변경

--- components/home/AllEvents.tsx
+++ components/home/AllEvents.tsx
@@ -1,17 +1,20 @@
-import { getAllEvents, getEvents } from "@/util/fetch-events";
+import { getAllEvents, getScrollEvents } from "@/util/fetch-events";
 import { dehydrate, QueryClient, useInfiniteQuery } from "@tanstack/react-query";
 import EventCard from "./EventCard";
 import { Box } from "@mui/material";
 import { AxiosError } from "axios";
-import { useRef } from "react";
-import { useObserver } from "@/hooks/useObserver";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function AllEvents() {
-  const bottom = useRef<HTMLDivElement>(null);
+  const { ref, inView } = useInView();
   // 무한 스크롤 구현
-  const { data, fetchNextPage, error, isError, status } = useInfiniteQuery<any, AxiosError>({
+  const { data, fetchNextPage, error, isError, status, isFetchingNextPage } = useInfiniteQuery<
+    any,
+    AxiosError
+  >({
     queryKey: ["events"],
-    queryFn: getEvents,
+    queryFn: getScrollEvents,
     getNextPageParam: (_, allPage) => {
       // 다음 페이지 계산
       const nextPage = (1 + allPage.length) * 12;
@@ -20,41 +23,43 @@ export default function AllEvents() {
     },
   });
 
-  const observed: IntersectionObserverCallback = ([entry]) =>
-    entry.isIntersecting && fetchNextPage();
-
-  useObserver({ target: bottom, callback: observed });
+  useEffect(() => {
+    // ref가 view에 들어오면 함수 실행
+    if (inView) fetchNextPage();
+  }, [inView]);
 
   if (isError) return <div>{error.message}</div>;
 
   if (status === "loading") return <p>Loading...</p>;
 
   return (
-    <Box
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        justifyContent: "center",
-        justifyItems: "center",
-        marginTop: 2,
-      }}
-    >
-      {/* 무한 스크롤 렌더링, 이중 배열 구조로 되어있다. */}
-      {data.pages.map((group) => {
-        return group.culturalEventInfo.row.map((event: any) => {
-          return (
-            <EventCard
-              key={event.TITLE}
-              title={event.TITLE}
-              date={event.DATE}
-              img={event.MAIN_IMG}
-            />
-          );
-        });
-      })}
-      <div ref={bottom} />
-      <button onClick={() => fetchNextPage()}>더 불러오기</button>
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          justifyContent: "center",
+          justifyItems: "center",
+          marginTop: 2,
+        }}
+      >
+        {/* 무한 스크롤 렌더링, 이중 배열 구조로 되어있다. */}
+        {data.pages.map((group) => {
+          return group.culturalEventInfo.row.map((event: any) => {
+            return (
+              <EventCard
+                key={event.TITLE}
+                title={event.TITLE}
+                date={event.DATE}
+                img={event.MAIN_IMG}
+              />
+            );
+          });
+        })}
+      </Box>
+      <div ref={ref} style={{ height: 50 }} />
+      {isFetchingNextPage ? <p>불러오는중...</p> : null}
+    </>
   );
 }
 
